4ad33ae8b6b0328b6d181587a74452d7491ac2c6
aaditya99l <12202321@students.koi.edu.au>
2025-05-27 13:59:33 +1000

    sync-aaditya

--- app/src/main/java/com/example/healthapp/GoogleFitManager.kt
+++ app/src/main/java/com/example/healthapp/GoogleFitManager.kt
@@ -1,13 +1,20 @@
+package com.example.healthmonitoring_app.android
+
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.android.gms.auth.api.signin.*
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.data.DataType
 import com.google.android.gms.fitness.request.DataReadRequest
 import com.google.android.gms.tasks.Tasks
+import java.util.*
 import java.util.concurrent.TimeUnit
-import java.util.Calendar
 
-class GoogleFitManager(private val context: Context) {
+class GoogleFitManager(private val context: Context) : Parcelable {
+
+    constructor(parcel: Parcel) : this(TODO("context")) {
+    }
 
     private fun getGoogleAccount(): GoogleSignInAccount? {
         return GoogleSignIn.getAccountForExtension(context, fitnessOptions())
@@ -39,11 +46,9 @@ class GoogleFitManager(private val context: Context) {
             Fitness.getHistoryClient(context, account).readData(request)
         )
 
-        val buckets = response.buckets
         var totalSteps = 0
-        for (bucket in buckets) {
-            val dataSets = bucket.dataSets
-            for (dataSet in dataSets) {
+        for (bucket in response.buckets) {
+            for (dataSet in bucket.dataSets) {
                 for (dp in dataSet.dataPoints) {
                     totalSteps += dp.getValue(DataType.TYPE_STEP_COUNT_DELTA.fields[0]).asInt()
                 }
@@ -53,7 +58,82 @@ class GoogleFitManager(private val context: Context) {
         return totalSteps
     }
 
-    // Similarly, you can add readHeartRate() and readSleepData() methods here
-}
+    fun readHeartRate(): Float {
+        val account = getGoogleAccount() ?: return -1f
 
+        val cal = Calendar.getInstance()
+        val endTime = cal.timeInMillis
+        cal.add(Calendar.DAY_OF_YEAR, -1)
+        val startTime = cal.timeInMillis
+
+        val request = DataReadRequest.Builder()
+            .read(DataType.TYPE_HEART_RATE_BPM)
+            .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+            .build()
+
+        val response = Tasks.await(
+            Fitness.getHistoryClient(context, account).readData(request)
+        )
+
+        var totalBpm = 0f
+        var count = 0
+
+        for (dataSet in response.dataSets) {
+            for (dp in dataSet.dataPoints) {
+                val bpm = dp.getValue(DataType.TYPE_HEART_RATE_BPM.fields[0]).asFloat()
+                totalBpm += bpm
+                count++
+            }
+        }
+
+        return if (count > 0) totalBpm / count else 0f
+    }
+
+    fun readSleepHours(): Float {
+        val account = getGoogleAccount() ?: return 0f
+
+        val cal = Calendar.getInstance()
+        val endTime = cal.timeInMillis
+        cal.add(Calendar.DAY_OF_YEAR, -1)
+        val startTime = cal.timeInMillis
+
+        val request = DataReadRequest.Builder()
+            .read(DataType.TYPE_SLEEP_SEGMENT)
+            .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+            .build()
+
+        val response = Tasks.await(
+            Fitness.getHistoryClient(context, account).readData(request)
+        )
+
+        var totalSleepMillis = 0L
+
+        for (dataSet in response.dataSets) {
+            for (dp in dataSet.dataPoints) {
+                val sleepStart = dp.getStartTime(TimeUnit.MILLISECONDS)
+                val sleepEnd = dp.getEndTime(TimeUnit.MILLISECONDS)
+                totalSleepMillis += (sleepEnd - sleepStart)
+            }
+        }
+
+        return totalSleepMillis / (1000f * 60f * 60f) // Convert ms to hours
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<GoogleFitManager> {
+        override fun createFromParcel(parcel: Parcel): GoogleFitManager {
+            return GoogleFitManager(parcel)
+        }
+
+        override fun newArray(size: Int): Array<GoogleFitManager?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
